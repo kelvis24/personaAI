02786a80aa6c15ebfa7bb9094bb7192cd8c05744
kelvis24 <elviskimara@outlook.com>
2024-02-23 16:50:12 -0600

    example for how to get all user data

--- other/models being tested/Python Web App for testing (flaskapp)/src/app.py
+++ other/models being tested/Python Web App for testing (flaskapp)/src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 import random
+from firebase import Firebase  # Import the Firebase class from firebase.py
 
 def FunctionToReplace(input_string):
     greetings = ["Hello!", "Hi there!", "Greetings!", "Good day!", "my boy", "Hey!"]
@@ -9,6 +10,8 @@ def FunctionToReplace(input_string):
 app = Flask(__name__)
 CORS(app)  # Enable CORS for your Flask app
 
+# Initialize Firebase
+firebase = Firebase("/Users/elviskimara/Downloads/new/personaAI/other/models being tested/Python Web App for testing (flaskapp)/src/personaai-d7d1a-firebase-adminsdk-4578u-a52d9e8d87.json")
 
 @app.route('/')
 def index():
@@ -23,10 +26,19 @@ def api():
     # Do something with the input/ function to replace / Load model to do inference then return the result
     result = FunctionToReplace(userInput)
     
-    # model = YOLO("app/src/ppe.pt")
-    return jsonify({"message": result})
+    # Call methods from Firebase class
+    document_data = firebase.read_from_DB("users", "8QMFyWIBQyI8PPbEmwMy")
+    collection_data = firebase.get_collection("users")
+    
+    ## TODO: Use this to get all user data like this: firebase.query_by_property("conversations", "username", "usernamePlaceholder"
+    query_data = firebase.query_by_property("users", "firstName", "elvis")
+    
+    return jsonify({
+        "message": result,
+        "document_data": document_data,
+        "collection_data": collection_data,
+        "query_data": query_data
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
